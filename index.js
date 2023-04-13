a004cdf793b56f1c454017b8918c03e23cad3048
matiscava <matiscava@hotmail.com>
2023-04-13 10:32:05 -0300

    Puse la validación del texto a descifrar

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 (() => {
+
+// Constantes 
   
 const $btnEncriptado = document.getElementById('btn-encriptar'),
   $btnDesncriptado = document.getElementById('btn-desencriptar'),
   $btnCopiar = document.getElementById('btn-copiar'),
   $textArea = document.getElementById('texto')
   $asideDivs = document.querySelectorAll('aside div');
-
-
-  $textArea.value = '';
-  const codigo = { 
+  
+const codigo = { 
     "e" : "enter",
     "i" : "imes", 
     "a" : "ai",  
@@ -16,18 +16,29 @@ const $btnEncriptado = document.getElementById('btn-encriptar'),
     "u" : "ufat"
   };
 
+const mayusculas = new RegExp('[A-Z]');
+const caracteresEspeciales = new RegExp('[À-ÿ]')
+  
+$textArea.value = '';
+
+// EVENTOS 
+
   $btnEncriptado.addEventListener( 'click', e => {
-    $asideDivs[1].querySelector('p').innerHTML = encriptar($textArea.value);
-    $textArea.value = '';
-    $asideDivs[0].classList.add('none');
-    $asideDivs[1].classList.remove('none');
+    if(!chequearTexto($textArea.value)){
+      $asideDivs[1].querySelector('p').innerHTML = encriptar($textArea.value);
+      $textArea.value = '';
+      $asideDivs[0].classList.add('none');
+      $asideDivs[1].classList.remove('none');
+    }
   })
 
   $btnDesncriptado.addEventListener( 'click', e => {
-    $asideDivs[1].querySelector('p').innerHTML = desencriptar($textArea.value);
-    $textArea.value = '';
-    $asideDivs[0].classList.add('none');
-    $asideDivs[1].classList.remove('none');
+    if(!chequearTexto($textArea.value)){
+      $asideDivs[1].querySelector('p').innerHTML = desencriptar($textArea.value);
+      $textArea.value = '';
+      $asideDivs[0].classList.add('none');
+      $asideDivs[1].classList.remove('none');
+    }
   })
 
   $btnCopiar.addEventListener( 'click' , e => {
@@ -38,28 +49,45 @@ const $btnEncriptado = document.getElementById('btn-encriptar'),
     $asideDivs[0].classList.remove('none');
   })
 
+// FUNCIONES
 
-
-  function encriptar(palabra) {
+  function encriptar(texto) {
     let encriptado = '';
-    for (let i = 0; i < palabra.length; i++) {
-      const letra = palabra.charAt(i);
+    for (let i = 0; i < texto.length; i++) {
+      const letra = texto.charAt(i);
       encriptado += codigo[letra] ? codigo[letra] : letra;
     }
     return encriptado;
   }
 
-  function desencriptar(palabra) {
+  function desencriptar(texto) {
     let desencriptado='';
     
     for (const letra in codigo) {
-      desencriptado = palabra.replace(codigo[letra], letra);
-      palabra = desencriptado;
+      desencriptado = texto.replace(codigo[letra], letra);
+      texto = desencriptado;
     }
     for (const letra in codigo) {
       if(desencriptado.includes(codigo[letra])) desencriptado = desencriptar(desencriptado);
     }
     return desencriptado;
   }
+
+  function chequearTexto(texto) {
+    if(texto.match(mayusculas) ) {
+      // alert("El texto tiene mayuscula, solo acepta minusculas. Modifique su texto");
+      $textArea.classList.add('error');
+      setTimeout(() => $textArea.classList.remove('error') ,300);
+      return true;
+    }
+    if(texto.match(caracteresEspeciales)){
+      // alert("El texto tiene carácteres especiales. Modifique su texto")
+      $textArea.classList.add('error');
+      setTimeout(() => $textArea.classList.remove('error') ,300);
+      return true;
+    }
+    return false;
+  }
+
 })()
 
